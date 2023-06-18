09e2be801458aaea2ba97798bd2c0cb0ac9e993a
Victor Chan <victorchan654@gmail.com>
2023-06-18 03:26:58 -0400

    Added crossOverImpact to the calculations

--- server.js
+++ server.js
@@ -39,6 +39,9 @@ wss.on('message', async function(message) {
     // "T": "n" newsEvent
     if(currentEvent.T === "n") { // This is a news event
         let companyImpact = 0;
+        let crossOverImpact = 0;
+        let companyWeight = 0.5;
+        let crossOverWeight = 0.5;
 
         // Ask ChatGPT its thoughts on the headline
         const apiRequestBody = {
@@ -68,8 +71,46 @@ wss.on('message', async function(message) {
         // Make trades based on the output (of the impact saved in companyImpact)
         const tickerSymbol = currentEvent.symbols[0];
 
+        let fastMA = 10;
+        let slowMA = 20;
+
+        let barset = await alpaca.getBars('day', tickerSymbol, {limit: slowMA});
+        let historicalPrices = barset[tickerSymbol].map(bar => bar.closePrice);
+
+        let fastMAValue = calculateMovingAverage(historicalPrices.slice(-fastMA));
+        let slowMAValue = calculateMovingAverage(historicalPrices);
+
+        // Check for MA crossover
+        if (fastMAValue > slowMAValue || fastMAValue < slowMAValue) {
+            // Moving Average crossover detected, ask ChatGPT for scoring
+            const apiRequestBody = {
+                "model": "gpt-3.5-turbo-16k",
+                "messages": [
+                    { role: "system", content: "We have detected a Moving Average crossover. Please provide a score from 1-100 that indicates the potential impact of this event on the stock price." }, 
+                    { role: "user", content: `The fast moving average has ${fastMAValue > slowMAValue ? 'crossed above' : 'crossed below'} the slow moving average for the stock ${tickerSymbol}. Please provide an impact score.` }
+                ]
+            }
+
+            let crossoverImpact = 0;
+            await fetch("https://api.openai.com/v1/chat/completions", {
+                method: "POST",
+                headers: {
+                    "Authorization": "Bearer " + process.env.OPENAI_API_KEY,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(apiRequestBody)
+            }).then((data) => {
+                return data.json();
+            }).then((data) => {
+                console.log(data);
+                crossoverImpact = parseInt(data.choices[0].message.content);
+            });
+        }
+
+        impact = companyImpact*companyWeight + crossOverImpact*crossOverWeight
+
         // 1 - 100, 1 being the most negative, 100 being the most positive impact on a company.
-        if(companyImpact >= 70) { // if score >= 70 : BUY STOCK
+        if(impact >= 70) { // if score >= 70 : BUY STOCK
             // Check for sufficient capital before buying
             const account = await alpaca.getAccount();
             if (account.cash < 0) {
@@ -90,12 +131,19 @@ wss.on('message', async function(message) {
                 console.error("Error placing order: ", err);
             }
 
-        } else if (companyImpact <= 30) { // else if impact <= 30: SELL ALL OF STOCK
+        } else if (impact <= 30) { // else if impact <= 30: SELL ALL OF STOCK
             // Sell stock
             try {
                 let closedPosition = alpaca.closePosition(tickerSymbol); //(tickerSymbol);
+            } catch (err) {
+                console.error("Error ")
             }
         }
         
     }
 });
+
+function calculateMovingAverage(prices) {
+    let sum = prices.reduce((a, b) => a + b, 0);
+    return sum / prices.length;
+}
